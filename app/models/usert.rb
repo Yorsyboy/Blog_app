10aef9e2ff3528ef1b9dd6ad3c60378e39018dde
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-08-11 22:13:35 +0100

    Add recent posts function

--- app/models/usert.rb
+++ app/models/usert.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_many :likes
     has_many :posts
     has_many :comments
+
+    def recent_posts(count: 3)
+        Post.where(user_id: id).order(created_at: :desc)[0, count]
+    end
 end
