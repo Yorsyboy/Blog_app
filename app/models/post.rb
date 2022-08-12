0656d1f08a1b92c7a39bd2a19979fb3ba0a8672a
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-08-12 23:59:56 +0100

    Unit test for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,14 +10,13 @@ class Post < ApplicationRecord
 
   after_save :increment_counter
 
-  private
+  
 
   def increment_counter
-    current_user = User.find(user_id)
-    current_user.update(posts_counter: (current_user.posts_counter || 0) + 1)
+    user.update(posts_counter: (user.posts_counter || 0) + 1)
   end
 
   def recent_comments(count: 5)
-    Comment.where(post_id: id).order(created_at: :desc)[0, count]
+    comments.order(created_at: :desc)[0, count]
   end
 end
