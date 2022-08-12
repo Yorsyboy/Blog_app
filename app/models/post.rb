2910437aca70ff9cafb37d191ba90b0647c0d361
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-08-12 08:01:12 +0100

    Add Post model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :comments
+
+  validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true  }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
+
   after_save :increment_counter
 
   private
