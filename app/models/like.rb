06acd5cc7f04ef918e06b0efda37592d130ebe1f
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-08-13 00:04:58 +0100

    Unit test for like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,10 +3,8 @@ class Like < ApplicationRecord
   belongs_to :post
   after_save :increment_counter
 
-  private
 
   def increment_counter
-    current_post = Post.find(post_id)
-    current_post.update(likes_counter: (current_post.likes_counter || 0) + 1)
+    post.update(likes_counter: (post.likes_counter || 0) + 1)
   end
 end
